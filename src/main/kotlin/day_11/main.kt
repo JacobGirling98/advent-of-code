c5ddb5f2991038a43ca95a4ec5c3a4793bdc98ad
JacobGirling98 <jacob.girling98@gmail.com>
2022-12-11 13:19:11 +0000

    day 11 part 2

--- src/main/kotlin/day_11/main.kt
+++ src/main/kotlin/day_11/main.kt
@@ -5,15 +5,20 @@ import java.io.File
 data class Monkey(
     val items: MutableList<Long>,
     val operation: (old: Long) -> Long,
-    val test: (old: Long) -> Boolean,
+    val test: Long,
     val testSuccessMonkey: Int,
     val testFailureMonkey: Int
 ) {
     var inspectionCount = 0
 
-    fun inspectItem(item: Long, successfulMonkey: Monkey, failedMonkey: Monkey, doesWorryLevelDecrease: Boolean) {
-        val inspectedItem = operation(item).let { if (doesWorryLevelDecrease) it / 3 else it }
-        if (test(inspectedItem)) {
+    fun inspectItem(
+        item: Long,
+        successfulMonkey: Monkey,
+        failedMonkey: Monkey,
+        modulateWorryLevel: (level: Long) -> Long
+    ) {
+        val inspectedItem = modulateWorryLevel(operation(item))
+        if (inspectedItem % test == 0L) {
             successfulMonkey.items.add(inspectedItem)
         } else {
             failedMonkey.items.add(inspectedItem)
@@ -35,7 +40,7 @@ fun String.toMonkey() = trimMargin().split("\n").let {
     Monkey(
         it[1].split("Starting items:")[1].split(",").map { item -> item.trim().toLong() }.toMutableList(),
         it[2].split("Operation: new = ")[1].split(" ").toOperation(),
-        it[3].split("Test: divisible by ")[1].let { value -> { old: Long -> old % value.toLong() == 0L } },
+        it[3].split("Test: divisible by ")[1].toLong(),
         it[4].split(" ").last().toInt(),
         it[5].split(" ").last().toInt()
     )
@@ -43,28 +48,35 @@ fun String.toMonkey() = trimMargin().split("\n").let {
 
 fun readInput() = File("src/main/kotlin/day_11/input.txt").readText().trimMargin().split("\n\n").map { it.toMonkey() }
 
-fun playARound(monkeys: List<Monkey>, doesWorryLevelDecrease: Boolean) {
+fun playARound(monkeys: List<Monkey>, modulateWorryLevel: (level: Long) -> Long) {
     monkeys.forEach {
         while (it.items.isNotEmpty()) {
             it.inspectItem(
                 it.items.first(),
                 monkeys[it.testSuccessMonkey],
                 monkeys[it.testFailureMonkey],
-                doesWorryLevelDecrease
+                modulateWorryLevel
             )
         }
     }
 }
 
 fun part1() = readInput()
-    .apply { repeat(20) { playARound(this, doesWorryLevelDecrease = true) } }
+    .apply { repeat(20) { playARound(this, modulateWorryLevel = { level -> level / 3 }) } }
     .sortedByDescending { it.inspectionCount }
     .take(2)
     .map { it.inspectionCount }
     .reduce { a, b -> a * b }
 
 fun part2() = readInput()
-    .apply { repeat(10000) { playARound(this, doesWorryLevelDecrease = false) } }
+    .apply {
+        repeat(10000) {
+            playARound(this, modulateWorryLevel = { level ->
+                level % this.map { it.test.toInt() }.reduce { a, b -> a * b }
+                    .toLong()
+            })
+        }
+    }
     .sortedByDescending { it.inspectionCount }
     .take(2)
     .map { it.inspectionCount }
